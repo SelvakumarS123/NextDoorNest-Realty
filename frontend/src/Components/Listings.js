914b02ab150884e1445215ce2c6dd42544760b2c
Selvakumar S <2002selvakumar@gmail.com>
2024-09-21 14:38:04 +0530

    After Many Failed Experiments
    Toolbar component and the four white horizontal lines ⚟

--- frontend/src/Components/Listings.js
+++ frontend/src/Components/Listings.js
@@ -1,73 +1,172 @@
-import React,{useState} from 'react'
-//leaflet
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import React, { useState } from 'react';
+// Leaflet
+import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline, Polygon } from 'react-leaflet';
 import { Icon } from "leaflet";
-//MUI
-import { Button, Typography, AppBar, Toolbar } from '@mui/material';
+// MUI
+import { Button, Typography, AppBar, Toolbar, Card, CardHeader, CardMedia, CardContent } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 // Map icons
 import houseIconPng from "./Assets/Mapicons/house.png";
 import apartmentIconPng from "./Assets/Mapicons/apartment.png";
 import officeIconPng from "./Assets/Mapicons/office.png";
-//Assets
+// Assets
 import img1 from "./Assets/img1.jpg";
 import myListings from "./Assets/Data/Dummydata";
 
 function Listings() {
- // const navigate = useNavigate();
-	const houseIcon = new Icon({
-		iconUrl: houseIconPng,
-		iconSize: [40, 40],
-	});
+   // const navigate = useNavigate();
+  const houseIcon = new Icon({
+    iconUrl: houseIconPng,
+    iconSize: [40, 40],
+  });
 
-	const apartmentIcon = new Icon({
-		iconUrl: apartmentIconPng,
-		iconSize: [40, 40],
-	});
+  const apartmentIcon = new Icon({
+    iconUrl: apartmentIconPng,
+    iconSize: [40, 40],
+  });
 
-	const officeIcon = new Icon({
-		iconUrl: officeIconPng,
-		iconSize: [40, 40],
-	});
-  const [latitude, setLatitude] = useState(10.549321138198273);
-	const [longitude, setLongitude] = useState(77.77617036973302);
-  function GoEast() {
-		setLatitude(10.543583547144538);
-		setLongitude(77.76839681674443);
-	}
-  function GoCenter() {
-		setLatitude(10.549321138198273);
-		setLongitude(77.77617036973302);
-	}
+  const officeIcon = new Icon({
+    iconUrl: officeIconPng,
+    iconSize: [40, 40],
+  });
+
+  const [latitude, setLatitude] = useState(51.48740865233002);
+  const [longitude, setLongitude] = useState(-0.12667052265135625);
+
+  	
+  // function GoEast() {
+        //         setLatitude(51.46567014039476);
+        //         setLongitude(0.2596173538795676);
+        // }
+  // function GoCenter() {
+        //         setLatitude(51.48740865233002);
+        //         setLongitude(-0.12667052265135625);
+        // }
+
+  const polyOne = [
+    [51.505, -0.09],
+    [51.51, -0.1],
+    [51.51, -0.12],
+  ];
+
+  const polygonOne = [
+    [51.515, -0.09],
+    [51.52, -0.1],
+    [51.52, -0.12],
+  ];
+
+  // Function to determine the icon based on listing type
+  function IconDisplay(listing) {
+    if (listing.listing_type === 'House') {
+      return houseIcon;
+    } else if (listing.listing_type === 'Apartment') {
+      return apartmentIcon;
+    } else if (listing.listing_type === 'Office') {
+      return officeIcon;
+    }
+    return null; // Fallback if none match
+  }
+
   return (
     <Grid container>
       <Grid item size={4}>
-        <Button onClick={GoEast}>GoEast</Button>
-        <Button onClick={GoCenter}>GoCenter</Button>
+        {myListings.map((listing) => {
+          return (
+            <Card sx={{
+              margin: '0.5rem',
+              border: '1px solid black',
+              position: 'relative',
+            }} key={listing.id}>
+              <CardHeader
+                  // action={
+                  //   <IconButton aria-label="settings">
+                  //     <MoreVertIcon />
+                  //   </IconButton>
+                  // }
+                title={listing.title}
+                subheader="September 14, 2016"
+              />
+              <CardMedia sx={{
+                paddingRight: '1rem',
+                paddingLeft: '1rem',
+                height: '20rem',
+                width: '32rem',
+              }}
+                component="img"
+                image={listing.picture1}
+                alt={listing.title}
+              />
+              <CardContent>
+                <Typography variant="body2">
+                  {listing.description.substring(0, 200)}...
+                </Typography>
+              </CardContent>
+              {listing.property_status === "Sale" ? (
+                  <Typography sx={{position:'absolute',backgroundColor:'green', zIndex:'1000', color:'white', top:'100px', left:'20px', padding:'5px'}}>
+                    {listing.listing_type}{" "}
+                    ${listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Typography>
+                ) : (
+                  <Typography sx={{position:'absolute',backgroundColor:'green', zIndex:'1000', color:'white', top:'100px', left:'20px', padding:'5px'}}>
+                    {listing.listing_type}{" "}
+                    ${listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}/{" "}{listing.rental_frequency}</Typography>)
+                }
+                {/* <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                </CardActions> */}
+            </Card>
+          );
+        })}
       </Grid>
-      <Grid item size={8}>
+      <Grid item size={8} style={{ marginTop: '0.5rem' }}>
         <AppBar position='sticky'>
-        <div style={{height:'100vh'}}>
-          <MapContainer center={[10.549321138198273, 77.77617036973302]} zoom={14} scrollWheelZoom={true}>
+          <Toolbar>
+          </Toolbar>
+
+          <div style={{ height: '100vh' }}>
+            <MapContainer center={[latitude, longitude]} zoom={14} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
+              <Polyline positions={polyOne} weight={5} color='green' />
+              <Polygon positions={polygonOne} color='yellow' fillColor='blue' fillOpacity={0.7} opacity={0}/>
+              {myListings.map((listing) => {
+                return (
+                  <Marker
+                    key={listing.id}
+                    icon={IconDisplay(listing)}
+                    position={[listing.location.coordinates[0], listing.location.coordinates[1]]}
+                  >
+                    <Popup>
+                      <Typography variant='h5'>{listing.title}</Typography>
+                      <img src={listing.picture1} style={{ height: '14rem', width: '18rem' }} alt={listing.title} />
+                      <Typography variant='body1'>{listing.description.substring(0, 150)}...</Typography>
+                      <Button variant='contained' fullWidth>Details</Button>
+                    </Popup>
+                  </Marker>
+                );
+              })}
               <Marker icon={houseIcon} position={[latitude, longitude]}>
                 <Popup>
                   <Typography variant='h5'>Title</Typography>
-                  <img src={img1} style={{height:'14rem', width:'18rem'}}/>
-                  <Typography variant='body1'> Some text below</Typography>
+                  <img src={img1} style={{ height: '14rem', width: '18rem' }} alt="Some description" />
+                  <Typography variant='body1'>Some text below</Typography>
                   <Button variant='contained' fullWidth>Link</Button>
                 </Popup>
               </Marker>
-          </MapContainer>
-        </div>
+            </MapContainer>
+          </div>
         </AppBar>
       </Grid>
     </Grid>
-    
-  )
+  );
 }
 
-export default Listings
+export default Listings;
+
+//sk
